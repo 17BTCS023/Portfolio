5e71071b81ed952b35f91ff0a0c74927867e6e9e
17BTCS023 <post.mk02@gmail.com>
2024-09-26 09:17:15 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <header>
+        <h1>MK</h1>
+        <nav>
+          <a href="">Github</a>
+          <a href="">Linkedin</a>
+        </nav>
+      </header>
+      <main>
+        <section>
+          <div>
+            <h1>Manisha Katiyar</h1>
+            <p>Tenacious working professional with an experience of almost 3 years in the space of IT Healthcare and 0.5 years in e-commerce. Skilled in web application development and proficient in MERN stack and React.js, I am passionate about leveraging technology to drive business growth and enhance user experiences. Eager to continue learning and contributing to innovative solutions in dynamic environments</p>
+          </div>
+          <div>
+                <img src="Portfolio\src\assets\Manisha-passport.jpg" alt="" />
+          </div>
+          <div>
+            <p>1</p>
+          </div>
+          <div>
+            <p>1</p>
+          </div>
+          <div>
+            <p>1</p>
+          </div>
+          <div>
+            <p>1</p>
+          </div>
+
+          <div>
+            <a href="">Email me!</a>
+          </div>
+
+        </section>
+
+        <section>
+          <h2>Latest Projects</h2>
+          <div>
+            
+          </div>
+        </section>
+      </main>
+
+
+
+    </>
+  )
+}
+
+export default App
